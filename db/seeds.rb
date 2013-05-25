a31f1e512173d8f3c8acdeccde49d5d7d4dfc562
Jacob Hart <jacobhart84@hotmail.com>
2013-05-24 22:59:06 -0500

    fixed industry nill  Y-Combinator

--- db/seeds.rb
+++ db/seeds.rb
@@ -118,7 +118,7 @@ companies.each_index do |i|
 
 
       if company_data["category_code"] == nil
-         c.industry_id = Industry.find_by_id(1)
+         c.industry_id = Industry.first.id
 # -------- Y-Combinator Industry is nil and won't show up / save as other
         elsif Industry.find_by_name(company_data["category_code"]) == nil
 
